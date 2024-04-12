184635189ccef34d52a0d965de6c2ebfbd7f0fae
Jan Gango <jangangolells@gmail.com>
2024-04-12 12:33:11 +0200

    Genero puesto

--- script.py
+++ script.py
@@ -10,18 +10,32 @@ ventana.title("Calculadora IMC")
 ventana.configure(background = "#9b9b9b")
 
 #Configuración fuentes
-FuenteTitulo = Font(family = "Montserrat", size = 26, weight = "bold")
-FuenteContexto = Font(family = "Montserrat",size = 13, weight = "normal" )
+FuenteTitulo = Font(family = "Montserrat", size = 27, weight = "bold")
+FuenteContexto = Font(family = "Montserrat",size = 12, weight = "normal")
+FuenteVentana = Font(family = "Montserrat", size = 16, weight= "normal")
+FuenteSeleccion = Font(family= "Montserrat", size = 14, weight= "normal")
+
+#Color
+ColorCaja = "#c4d2e7"
 
 #Canva
-canva = Canvas(width = 380, height = 200, bg = "blue")
-canva.place(x = 150, y = 180)
+frame = tkinter.Frame(ventana, width=460, height=200)
+frame.place(x = 70, y = 140)
+frame.config(bg= ColorCaja)
+frame.config(bd=25, relief="solid", borderwidth=4)
+
 #Titulo
 Titulo = tkinter.Label(ventana, text = "Calculadora de IMC", font=FuenteTitulo, background = "#9b9b9b")
 Titulo.place(x = 142, y = 20)
 
 #Descripción
 Contexto = tkinter.Label(ventana, text = "Descubre tu indice IMC para saber si estas como una vaca (o no)", background = "#9b9b9b", font = FuenteContexto)
-Contexto.place(x=63,y=65)
+Contexto.place(x=77,y=65)
+
+#Edad y genero
+Genero = tkinter.Label(ventana, text = "Género", font= FuenteVentana, bg = ColorCaja)
+Genero.place(x=95, y=165)
+GeneroElegir = ttk.Combobox(ventana, state="readonly", values=["Hombre", "Mujer"], width= 8, font=FuenteSeleccion)
+GeneroElegir.place(x = 180, y = 166)
 
 ventana.mainloop()
