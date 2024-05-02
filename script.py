7e857d84731dbcda46133a7c54217d5859159004
Jan Gango <jangangolells@gmail.com>
2024-05-02 14:18:42 +0200

    Agregando menú

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#Importar Librerias
 import tkinter
 from tkinter import *
 from tkinter import ttk
@@ -9,16 +10,29 @@ ventana.geometry("600x400")
 ventana.title("Calculadora IMC")
 ventana.configure(background = "#9b9b9b")
 
+#Configuración menú
+barra_menus = tkinter.Menu()
+menu_archivo = tkinter.Menu(barra_menus, tearoff=False)
+menu_archivo.add_command(
+    label="Nuevo",
+    accelerator="Ctrl+N",
+    compound=tkinter.LEFT
+)
+barra_menus.add_cascade(menu=menu_archivo, label="Archivo")
+ventana.config(menu=barra_menus)
+
+
 #Configuración fuentes
 FuenteTitulo = Font(family = "Montserrat", size = 27, weight = "bold")
 FuenteContexto = Font(family = "Montserrat",size = 12, weight = "normal")
 FuenteVentana = Font(family = "Montserrat", size = 16, weight= "normal")
 FuenteSeleccion = Font(family= "Montserrat", size = 14, weight= "normal")
+FuenteResultado = Font(family = "Montserrat", size = 18, weight= "normal")
 
-#Color
+#Configuración Colores
 ColorCaja = "#c4d2e7"
 
-#Función
+#Función Principal
 def Calcular():
     P = int(PesoElegir.get())
     A = float(AlturaElegir.get())
@@ -27,10 +41,10 @@ def Calcular():
     print(Resultado)
     ResultadoTexto = tkinter.StringVar()
     ResultadoTexto.set(f"Tu IMC es de " + str(Resultado))
-    TextoResultado = tkinter.Label(ventana, textvariable=ResultadoTexto)
-    TextoResultado.place(x = 234, y = 290)
+    TextoResultado = tkinter.Label(ventana, textvariable=ResultadoTexto, font=FuenteResultado)
+    TextoResultado.place(x = 220, y = 350)
 
-#Canva
+#Fondo
 frame = tkinter.Frame(ventana, width=460, height=200)
 frame.place(x = 70, y = 140)
 frame.config(bg= ColorCaja)
@@ -42,16 +56,20 @@ Titulo = tkinter.Label(ventana, text = "Calculadora de IMC", font=FuenteTitulo, 
 #Descripción
 Contexto = tkinter.Label(ventana, text = "Descubre tu indice IMC para saber si estas como una vaca (o no)", background = "#9b9b9b", font = FuenteContexto).place(x=77,y=65)
 
-#Peso y altura
-Peso = tkinter.Label(ventana, text = "Peso", font = FuenteVentana, bg= ColorCaja).place(x=103, y = 210)
+#Peso
+Peso = tkinter.Label(ventana, text = "Peso", font = FuenteVentana, bg= ColorCaja).place(x=103, y = 180)
 PesoElegir = tkinter.Entry(ventana,width=10, font = FuenteSeleccion)
-PesoElegir.place(x=180, y= 210)
-#--#
-Altura = tkinter.Label(ventana, text = "Altura",font = FuenteVentana, bg = ColorCaja).place(x=320, y = 205)
+PesoElegir.place(x=165, y= 180)
+
+
+#Altura
+Altura = tkinter.Label(ventana, text = "Altura",font = FuenteVentana, bg = ColorCaja).place(x=320, y = 180)
 AlturaElegir = tkinter.Entry(ventana, width=10, font = FuenteSeleccion)
-AlturaElegir.place(x=385, y= 208)
+AlturaElegir.place(x=385, y= 180)
+
 
 #Botón
-Boton = ttk.Button(ventana, text = "Calcula", width = 20,command= Calcular).place(x = 234, y = 260)
+Boton = ttk.Button(ventana, text = "Calcula", width = 20,command= Calcular).place(x = 234, y = 230)
+
 
 ventana.mainloop()
